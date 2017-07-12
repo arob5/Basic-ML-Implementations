5fc3a6b7ed487b3a41ef13e7f72a8f6aa9c15297
Andrew Roberts <andrewroberts@Andrews-MBP-29.wowway.com>
2017-07-11 21:54:50 -0500

    Forward prop now working - need to add back prop

--- xor_forward_prop.py
+++ xor_forward_prop.py
@@ -13,11 +13,19 @@ def main():
 	""" Runs helper functions """
 
 	n_neurons_each_layer = [2, 3, 1] 
-
 	X, y = get_input()
-	weights = initialize_weights(n_neurons_each_layer[0], n_neurons_each_layer[1])
-	signal = calculate_signal(input, weights)
-	activation = activate_signal(input, weights) 
+	activation = X 
+
+	for i in range(0, NUM_LAYERS-1): 
+		print activation
+		weights = initialize_weights(n_neurons_each_layer[i], n_neurons_each_layer[i+1])
+		print weights
+		signal = calculate_signal(activation, weights)
+		print signal
+		activation = sigmoid(signal) 
+		print activation
+
+	print "Output: ", activation
 
 def get_input():
 	""" 
@@ -32,6 +40,11 @@ def get_input():
 	
 	return X, y.T
 
+def calculate_signal(input, weights): 
+	""" Return dot product of feature matrix and weights transpose """
+
+	return np.dot(input, weights.T)
+
 def initialize_weights(n_neurons_current, n_neurons_next): 
 	""" 
 	Returns k x n matrix of random weights in interval [-1.0, 1.0) 
@@ -40,7 +53,6 @@ def initialize_weights(n_neurons_current, n_neurons_next):
 
 	Hidden layer will have 3 neurons    
 	""" 
-
 	np.random.seed(42)
 	return 2*np.random.random((n_neurons_next, n_neurons_current)) - 1 
 
@@ -50,17 +62,9 @@ def sigmoid(signal):
 	Will do elementwise operation on matrices
 	"""
 
-	exponential = -1 * np.exp(signal)
+	exponential = np.exp(signal * -1)
 	denominator = np.add(exponential, 1)
 
 	return 1 / denominator 
-
-
-
-
-
-
-	
-
 
 main()
