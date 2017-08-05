8c65ed87c1896e043d7c24a45c1fea7009132136
Andrew Roberts <andrewroberts@Andrews-MBP-29.wowway.com>
2017-08-05 17:45:15 -0500

    Added function to calculate values in markov_class

--- reinforcement_learning/markov_class.py
+++ reinforcement_learning/markov_class.py
@@ -1,7 +1,7 @@
 #
 # markov_class.py
 # A class defining a Markov Chain and a second class defining a Markov Reward Process that inherits from the first class
-# Last Modified: 8/3/2017
+# Last Modified: 8/5/2017
 # Modified By: Andrew Roberts
 #
 
@@ -9,8 +9,14 @@ import numpy as np
 
 class MarkovChain():
 	def __init__(self, P, labels):
-		self.P = P
-		self.labels = labels
+		try:
+			if (P.shape[0] != len(labels)) or (P.shape[1] != len(labels)):
+				raise Exception
+		except Exception:
+			print("Invalid input: Dimensions of transition matrix must match label length")
+		else:
+			self.P = P
+			self.labels = labels
 	
 	def sample_episode(self, start_state):
 		"""
@@ -47,9 +53,18 @@ class MarkovChain():
 class MRC(MarkovChain):
 	def __init__(self, P, labels, rewards):
 		super().__init__(P, labels)
-		self.rewards = rewards
+		self.values = None
+		
+		try: 
+			if len(rewards) != len(self.labels):
+				raise Exception
+		except Exception:
+			print("Length of reward array must match length of label array")
+		else:
+			self.rewards = rewards 
 
 	def calc_sample_reward(self, sample):
+		""" Returns the total reward of a sample episode passed in as argument"""
 		reward_dict = dict(zip(self.labels, self.rewards))
 
 		reward_total = 0
@@ -58,15 +73,35 @@ class MRC(MarkovChain):
 		
 		return reward_total		
 
+	def store_values(self, discount):
+		""" 
+		Stores the values of each state as an instance variable
+		Value function defined as expected return given a state    	
+		Finds values by solving the vectorized Bellman Equation
+		Will only work if matrix is invertible 
+		
+		Argument: A discount factor; 0 <= discount <= 1
+		"""
+
+		try:
+			if (discount < 0) or (discount > 1):
+				raise Exception
+		except Exception:
+			print("Input Error: Discount value must be between 0 and 1 (inclusive)")
+		else: 
+			I = np.identity(n=len(self.labels))	
+			self.values = np.linalg.inv(I - (discount * self.P)) @ self.rewards.reshape(-1, 1)
 
 
 
-"""
 states = ["Enroll", "Class", "Drop", "Permanent Drop", "Graduate", "End"]
+rewards = [1, 5, -5, -20, 20, 0]
 P = np.array([[0, .8, .1, .1, 0, 0], [0, .7, .1, .1, .1, 0], [0, .3, .4, .3, 0, 0], [0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1]])
 
-mc = MarkovChain(P, states)
+mc = MRC(P, states, rewards)
+print(mc.store_values(1))
 
-for i in range(10):
-	print(mc.sample_episode("Enroll"))
-"""
+
+
+#for i in range(10):
+#	print(mc.sample_episode("Enroll"))
